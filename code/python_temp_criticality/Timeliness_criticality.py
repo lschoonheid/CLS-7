09019c52f6149a514e143c025210985204eb7fce
Laszlo Schoonheid <lschoonheid@gmail.com>
2026-02-01 23:27:27 +0100

    fix imports

--- code/python_temp_criticality/Timeliness_criticality.py
+++ code/python_temp_criticality/Timeliness_criticality.py
@@ -2,8 +2,16 @@
 import sys
 import os
 
-sys.path.append(os.path.abspath('../../../timeliness_criticality_paper/python_temp_criticality/python_real_world_networks'))
+sys.path.append(
+    os.path.abspath("../../../timeliness_criticality_paper/python_temp_criticality/python_real_world_networks")
+)
 from DelayBufferNetwork import DelayBufferNetwork
 
-sys.path.append(os.path.abspath('../../../timeliness_criticality_paper/python_temp_criticality/python_synthetic_temporal_networks'))
-from stn_simulations import synthetic_temporal_network, synthetic_temporal_network_heterogeneous_K, synthetic_temporal_network_sparsity
+sys.path.append(
+    os.path.abspath("../../../timeliness_criticality_paper/python_temp_criticality/python_synthetic_temporal_networks")
+)
+from stn_simulations import (
+    synthetic_temporal_network,
+    synthetic_temporal_network_heterogeneous_K,
+    synthetic_temporal_network_sparsity,
+)
---
+++ code/python_temp_criticality/__init__.py
@@ -0,0 +1,9 @@
+from Timeliness_criticality import (
+    DelayBufferNetwork,
+    synthetic_temporal_network,
+    synthetic_temporal_network_heterogeneous_K,
+    synthetic_temporal_network_sparsity,
+)
+
+__version__ = "1.0.0"
+__author__ = "Timeliness Criticality Study"
